5019bf44ab602cb5c4c91cea7e2091fe5d973ffe
niclsw <nicklswx@gmail.com>
2022-03-15 20:48:11 -0500

    lab2

---
+++ Lab 2/LinkedStack.h
@@ -0,0 +1,6 @@
+#ifndef LINKED_STACK_
+#define LINKED_STACK_
+
+
+#include "LinkedStack.cpp"
+#endif
--- Lab 2/main.cpp
+++ Lab 2/main.cpp
@@ -5,6 +5,23 @@
 
 int main() {
 
+    std::string fileName{""};
+    std::fstream file;
+
+    // Do while file error
+    do {
+        // Accept user input for file name
+        std::cout << "Enter File Name: ";
+        std::cin >> fileName;
+
+        // Open the file
+        file.open(fileName, std::ios::in);
+
+        // Display error if error opening file
+        if(!file) { std::cout << "Error opening file" << std::endl; }
+    } while (!file);
+
+
     return 0;
     
 }
