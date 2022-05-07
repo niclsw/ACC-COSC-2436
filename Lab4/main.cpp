0c95115a9303d818376e14adea0c15d7d25cc491
Nick Walker <nicklswx@gmail.com>
2022-05-07 14:11:38 -0500

    finally works maybe

--- Lab4/main.cpp
+++ Lab4/main.cpp
@@ -2,35 +2,53 @@
 #include <fstream>
 #include "BinaryNodeTree.h"
 
+std::fstream getFile();
+void readToTree(BinaryNodeTree &tree, std::fstream &file);
+
 int main() {
 
     BinaryNodeTree tree;
-
-    std::string name1 = "Alpha";
-    std::string name2 = "Beta";
-    std::string name3 = "Gamma";
-    std::string name4 = "Delta";
-
-    int id1 = 1;
-    int id2 = 2;
-    int id3 = 3;
-    int id4 = 4;
+    std::fstream file = getFile();
     
 
-    tree.insert(name1, id1);
-    tree.insert(name2, id2);
-    tree.insert(name3, id3);
-    tree.insert(name4, id4);
 
 
-    std::cout << "Tree: " << std::endl;
 
-    if (tree.find(id1)) {
-        std::cout << "Found " << id1 << std::endl;
-    }
-    else {
-        std::cout << "Not found " << id1 << std::endl;
-    }
+    tree.print();
 
     return 0;
 }
+
+////////////////////////////////////////////////////////////////////////////////
+// Name:       getFile
+// Inputs:     std::string &fileName
+// Outputs:    std::fstream
+// Purpose:    Used to get file name from user and error check file open. The file
+//             is then returned to the calling function.
+////////////////////////////////////////////////////////////////////////////////
+
+std::fstream getFile(std::string &fileName)
+{
+    // initialize file stream
+    std::fstream file;
+
+    // get file name from user
+    std::cout << "Enter file name: ";
+    std::cin >> fileName;
+
+    // open file
+    file.open(fileName, std::ios::in);
+
+    // Error check the file open. If it fails, print an error message.
+    try
+    {
+        if (!file)
+            throw std::invalid_argument("File failed to open");
+    }
+    catch (std::invalid_argument &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+
+    return file;
+} // end getFile
